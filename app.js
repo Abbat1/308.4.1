4e90fe9ea034f0d08a633fd3b19f8ee3078c0c41
Abbat1 <cozzyyist@gmail.com>
2024-05-21 07:38:10 -0500

    308.4.1

---
+++ app.js
@@ -0,0 +1,9 @@
+/* Step 1. To copy and paste code used for csv file in previous lab
+Step 2.  using the information from the previous lab and place information
+into a string. From here you can adress it as you please.
+Step 3. then you need to target the /n. with given function,
+once it detects/n, to move to next row.
+*/
+/* OBjective of assignment.
+To have /n and a comma to indicate th ecoe should start on a new line. while also properly
+storing each "cell"/ bundle of info in a varibale.*/
