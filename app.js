a7ab07af96002f5e332c2cc71b5de4394a403123
Abbat1 <cozzyyist@gmail.com>
2024-06-14 20:40:21 -0500

    308.4.1-lab

--- app.js
+++ app.js
@@ -1,9 +1,66 @@
-/* Step 1. To copy and paste code used for csv file in previous lab
-Step 2.  using the information from the previous lab and place information
-into a string. From here you can adress it as you please.
-Step 3. then you need to target the /n. with given function,
-once it detects/n, to move to next row.
-*/
-/* OBjective of assignment.
-To have /n and a comma to indicate th ecoe should start on a new line. while also properly
-storing each "cell"/ bundle of info in a varibale.*/
+let n1 = 23;
+let n2 = 20;
+let n3 = 4;
+let n4 = 3;
+
+let isSum50 = (n1 + n2 + n3 + n4) == 50
+let isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
+let isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
+let isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
+let isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
+console.log(isValid);
+if (n1 % 5 === 0 && n4 % 5 === 0)
+   {console.log ("yessss!")}
+ else 
+   {console.log ("waaa!")}
+if (n2 % 5 === 0 || n3 % 5 === 0)
+   {console.log ("omgg!")}
+ else
+    {console.log ("boo!")} 
+if (n1 > n4)
+   {console.log ("woohoo!")}
+ else
+  {console.log (":/")}  
+let value = (((n1 - n2) * n3) % n4)
+console.log(value)
+//total distance 
+const distance = 1500;
+//miles per gallon at certain speeds
+const mph55 = 30;
+const mph60 = 28;
+const mph75 = 23;
+//miles driven per hour
+const firstTripmiles = 55;
+const secondTripmiles = 60;
+const thirdTripmiles = 75;
+//budgeting
+const budget = 175;
+//cost of fuel
+const fuelCost = 3;
+//amount of fuel used at certain speed
+const firstSpeed = (distance / mph55);
+const secondSpeed = (distance / mph60);
+const thirdSpeed = (distance / mph75);
+//will money be enough to travel at this speed?
+const firstTrip = (firstSpeed * fuelCost <= budget)
+const secondTrip = (secondSpeed * fuelCost <= budget)
+const thirdTrip = (thirdSpeed * fuelCost <= budget)
+//how long will the trip take in hours time 
+const firstTripHours = (distance / firstTripmiles)
+const secondTripHours = (distance / secondTripmiles)
+const thirdTripHours = (distance / thirdTripmiles)
+console.log(
+    `It will take ${firstSpeed} gallons of fuel going at 55 mph.
+    It will take ${secondSpeed} gallons of fuel going at 60 mph.
+    It will take ${thirdSpeed} gallons of fuel going at 75 mph.`
+)
+console.log(
+    `first trip budget is: ${firstTrip}
+    second trip budget is: ${secondTrip}
+    third trip budget is: ${thirdTrip}`
+)
+console.log(
+    `The first trip would be ${firstTripHours} hours.
+    The second trip would be ${secondTripHours} hours.
+    The third trip would be ${thirdTripHours} hours.`
+)
